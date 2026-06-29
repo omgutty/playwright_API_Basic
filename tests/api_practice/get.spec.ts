e48ef6403813658666c48aec11a5c064c8d052a9
omgutty <om.gutty@gmail.com>
2026-06-29 20:29:14 +0530

    feat: First test of API Practice

---
+++ tests/api_practice/get.spec.ts
@@ -0,0 +1,5 @@
+import { APIRequestContext } from '@playwright/test';
+export {test, expect,APIRequestContext} from '@playwright/test'
+
+const baseURL='https://restful-booker.herokuapp.com/booking/:id';
+
