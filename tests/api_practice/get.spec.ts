579f9f18e5d05965f335f248a7e36256230a7f40
omgutty <om.gutty@gmail.com>
2026-06-30 12:27:16 +0530

    feat: second test vaildating the porperty key in response

--- tests/api_practice/get.spec.ts
+++ tests/api_practice/get.spec.ts
@@ -35,11 +35,35 @@ test('get all product list',async ({request})=>{
 
 test('get the first product by id ', async ({request})=>{
     const response= await request.get(`${baseURL}/productsList`);
+   
+    //verifying the status code of the response 
     expect(response.status()).toBe(200);
+    //fetching the response body 
     const responsebody=await response.json();
+    //verifying the response body is grater than one record
+    expect(responsebody.products.length).toBeGreaterThan(0);
+    //printing the response status which is present in the response with key value 
     console.log(`Response body response code is `, await responsebody.responseCode);
+    //verifying the response code which is present in the body 
     expect (await responsebody.responseCode).toBe(200);
     
+    //fetching the product 1 with array, as response is in array with [0]
     const firstproduct = await responsebody.products[0];
-    console.log(firstproduct);
+    //printing the first product 
+    console.log(firstproduct); //it prints in on line if any nested object
+    //verifying the property key in response 
+    expect(firstproduct).toHaveProperty('id');
+    expect(firstproduct).toHaveProperty('name');
+    expect(firstproduct).toHaveProperty('price');
+    expect(firstproduct).toHaveProperty('brand');
+    expect(firstproduct).toHaveProperty('category');
+
+    //category is nested object which is printing in one line.
+    //so using stringfy 
+    // this print the nested object with another object with space 
+    const firstproductString=JSON.stringify(firstproduct, null,2)
+    console.log(firstproductString); 
+    const firstproductjson= JSON.parse(firstproductString)
+    console.log(firstproductjson);
+    
 })
