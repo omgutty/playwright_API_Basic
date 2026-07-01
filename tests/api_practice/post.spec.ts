318acabacf5b17d65074bf06a30bdbb87c57462b
omgutty <om.gutty@gmail.com>
2026-07-01 13:09:21 +0530

    feat: Post new record in booker app

--- tests/api_practice/post.spec.ts
+++ tests/api_practice/post.spec.ts
@@ -2,6 +2,8 @@ import {test,expect,request, APIRequestContext} from '@playwright/test'
 
 const baseURL='https://restful-booker.herokuapp.com';
 
+
+
 test('post auth', async ({request}:{request:APIRequestContext})=>{
     const response= await request.post(`${baseURL}/auth`, {
         headers:{
@@ -24,4 +26,31 @@ test('post auth', async ({request}:{request:APIRequestContext})=>{
     console.log(token);
 
     
+})
+
+
+test(`Creates a new booking in the API`, async({request})=>{
+    const response= await request.post(`${baseURL}/booking`,{
+        headers:{
+            'Content-Type':'application/json'
+        },
+        data:{
+            'firstname':'Om',
+            'lastname':'Gutty',
+            "totalprice" : 111,
+             "depositpaid" : true,
+            "bookingdates" : {
+                    "checkin" : "2026-01-01",
+                    "checkout" : "2027-01-01"
+            },
+            "additionalneeds" : "Breakfast"
+        }
+    });
+    const responsebody= await response.json();
+    expect(response.status()).toBe(200);
+    const bookingid= responsebody.bookingid;
+    expect(responsebody.bookingid).toBeDefined();
+    console.log(bookingid);
+    expect(bookingid).toEqual(expect.any(Number));
+
 })
